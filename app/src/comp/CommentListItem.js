12f76c95a59e6399fe1e53c8bb1a6b849e0648bc
Eunsun Mota <eunsun.mota@gmail.com>
2024-06-23 21:15:09 -0700

    remove button node in the list item feed

--- app/src/comp/CommentListItem.js
+++ app/src/comp/CommentListItem.js
@@ -3,7 +3,6 @@ import { formatDateTime } from "../util/DateFormatter";
 function CommentListItem({ data }) {
   return (
     <li className="comment-list-item" role="comment">
-      <button aria-labelledby={`comment-${data.id}`} />
       <article id={`comment-${data.id}`}>
         <p>{data.message}</p>
         <div>
