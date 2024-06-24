991cf4e9f9be3d5e97d44f0b18dc7e26627b654d
Eunsun Mota <eunsun.mota@gmail.com>
2024-06-23 21:05:36 -0700

    brief testing, not much

--- app/src/App.test.js
+++ app/src/App.test.js
@@ -1,8 +1,33 @@
-import { render, screen } from "@testing-library/react";
+import {render, fireEvent, screen} from '@testing-library/react'
 import App from "./App";
 
-test("renders learn react link", () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test("renders the comment list container with no error message", () => {
+  const result = render(<App />);
+  const listElement = result.container.querySelector(".comment-list");
+  expect(listElement).toBeInTheDocument();
+
+  const errorMessageEl = result.container.querySelector(".error-message");
+  expect(errorMessageEl).not.toBeInTheDocument();
+});
+
+test("renders the comment form", () => {
+  const result = render(<App />);
+  const formElement = result.container.querySelector(".comment-form form");
+  expect(formElement).toBeInTheDocument();
+});
+
+test("post a comment", () => {
+  const result = render(<App />);
+  const formElement = result.container.querySelector(".comment-form form");
+  expect(formElement).toBeInTheDocument();
+
+  const nameInput = result.container.querySelector(".comment-form form #name");
+  const commentInput = result.container.querySelector(".comment-form form #comment");
+  const submitBtn = result.container.querySelector(".comment-form form button[type='submit']");
+
+  // FILL OUT THE FORM AND SUBMIT
+  // nameInput.setAttribute("value", "Tester");
+  // commentInput.setAttribute("value", "This is a testing comment");
+  // fireEvent.click(submitBtn);
+  // TODO: NEED TO INTERCEPT THE API, will use nock maybe? 
 });
